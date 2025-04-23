1e4fb3f5fbd8361b0f8e5b0d0917749486b08190
ニンテン <awsomestronhero@gmail.com>
2025-04-23 18:39:16 +0100

    Player Movement is implimented

--- Coding-Fundementals/Assets/Scripts/Player_Movement.cs
+++ Coding-Fundementals/Assets/Scripts/Player_Movement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    public float Speed = 5;
+    private Rigidbody2D Rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+       float Move_X = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(Move_X, 0);
+
+      Rb.velocity = movement * Speed;
     }
 }
